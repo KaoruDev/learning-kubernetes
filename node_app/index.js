39bb475f4ddf16c2d8f933575162f7f15f6ed8f5
Kaoru Kohashigawa <dev@kaoruk.com>
2018-02-19 21:59:15 -0800

    create deployment yaml configs

--- node_app/index.js
+++ node_app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! from V2 \n');
 });
 
 var server = app.listen(3000, () => {
